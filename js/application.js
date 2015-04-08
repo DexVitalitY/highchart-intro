66ae60c1b414ed76b89b047e1149f7096313c2b4
Michael Chow <DexVitalitY@Michaels-MacBook-Pro.local>
2015-04-08 10:55:43 +0800

    Updated Application with ajax GET

--- js/application.js
+++ js/application.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
 
+function getRetailGas() {
+  $.ajax({
+      type: 'GET',
+      url: 'https://www.quandl.com/api/v1/datasets/BTS_MM/RETAILGAS.json',
+      dataType: 'json',
+      success: function(response){
+      console.log(response);
+      }
+    });
+
+  }
+getRetailGas();
+
+
 });
